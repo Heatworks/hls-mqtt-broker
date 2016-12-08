157b6953a0b9e921e90fe255ee71017bb7044f64
Weston Catron <westoncatron@Westons-MacBook-Pro.local>
2016-12-08 15:42:48 -0500

    Setup authentication.

--- index.js
+++ index.js
@@ -30,11 +30,6 @@ var moscaSettings = {
 var server = new mosca.Server(moscaSettings);   //here we start mosca
 server.on('ready', setup);  //on init it fires up setup()
 
-// fired when the mqtt server is ready
-function setup() {
-  console.log('Mosca server is up and running')
-}
-
 server.on('clientConnected', function(client) {
     console.log('client connected', client.id);
 });
@@ -43,3 +38,30 @@ server.on('clientConnected', function(client) {
 server.on('published', function(packet, client) {
   console.log('Published', packet.payload);
 });
+
+// Accepts the connection if the username and password are valid
+var authenticate = function(client, username, password, callback) {
+  var authorized = (username === 'alice' && password.toString() === 'secret');
+  if (authorized) client.user = username;
+  callback(null, authorized);
+}
+
+// In this case the client authorized as alice can publish to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+var authorizePublish = function(client, topic, payload, callback) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+
+// In this case the client authorized as alice can subscribe to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+var authorizeSubscribe = function(client, topic, callback) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+  server.authenticate = authenticate;
+  server.authorizePublish = authorizePublish;
+  server.authorizeSubscribe = authorizeSubscribe;
+}
